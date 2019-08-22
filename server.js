5bd1895f845e8344b08d607452c67b67bcdf06b6
Загидуллин Р.Н <r.zagidullin@ios.icl>
2019-08-22 17:04:01 +0300

    working beta 1.0.1

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-const express = require('express'),
-  app = express(),
-  http = require('http').Server(app),
-  io = require('socket.io')(http),
-  db = require('./queries'),
-  connectedUsers = {};
+const app = require('express')(),
+      http = require('http').Server(app),
+      io = require('socket.io')(http),
+      db = require('./queries'),
+      connectedUsers = {};
 
 io.on('connection', socket => {
   socket.on('name', userId => {
     connectedUsers[userId] = socket;
-    console.log(userId + ' connected');
 
     db.getUserNames().then(userList => {
-      console.log(userList);
-
       userList.forEach(user => {
         user.connected = !!connectedUsers[user.userid];
       });
@@ -24,36 +20,31 @@ io.on('connection', socket => {
   });
 
   socket.on('userList loaded', userId => {
+    db.getUserNames()
+      .then(users => {
+        for (let i = 0; i < users.length; i++) {
+          const user = users[i];
 
-    db.getUserNames().then(users => {
-      for (let i = 0; i < users.length; i++) {
-        const user = users[i];
-
-        if (user.userid === userId) {
-          continue;
-        }
-
-        db.getMessages(userId, user.userid, 0).then(messages => {
-          console.log('user ', user.userid, messages);
-
-          if (messages.length) {
-            connectedUsers[userId].emit('old messages', messages);
+          if (user.userid === userId) {
+            continue;
           }
-        });
-      }
-    }).catch(err => console.log(err));
+
+          db.getMessages(userId, user.userid, 0).then(messages => {
+            if (messages.length) {
+              connectedUsers[userId].emit('old messages', messages);
+            }
+          });
+        }
+      })
+      .catch(err => console.log(err));
 
     db.getUnseenMessages(userId).then(messages => {
-      console.log('unseen', messages);
-
       if (messages.length) {
         connectedUsers[userId].emit('unseen messages', messages);
       }
     });
 
     db.getMessages('', '_chat', 0).then(messages => {
-      console.log('chat', messages);
-
       if (messages.length) {
         connectedUsers[userId].emit('old messages', messages);
       }
@@ -65,16 +56,12 @@ io.on('connection', socket => {
   });
 
   socket.on('chat message', msg => {
-    console.log(msg);
-
     msg.id = new Date().getTime();
     db.createMessage(msg);
     socket.broadcast.emit('chat message', [msg]);
   });
 
   socket.on('private message', msg => {
-    console.log(msg);
-
     msg.id = new Date().getTime();
     db.createMessage(msg);
 
@@ -84,14 +71,13 @@ io.on('connection', socket => {
   });
 
   socket.on('get old', msg => {
-    const {userId, companionId, offset} = msg;
+    const { userId, companionId, offset } = msg;
 
-      db.getMessages(userId, companionId, offset).then(messages => {
-
-        if (messages.length) {
-          connectedUsers[userId].emit('old messages', messages);
-        }
-      });
+    db.getMessages(userId, companionId, offset).then(messages => {
+      if (messages.length) {
+        connectedUsers[userId].emit('old messages', messages);
+      }
+    });
   });
 
   socket.on('disconnect', () => {
@@ -99,8 +85,6 @@ io.on('connection', socket => {
       if (connectedUsers[userId] === socket) {
         delete connectedUsers[userId];
         io.emit('user disconnected', userId);
-
-        console.log(userId, ' disconnected');
       }
     }
   });
